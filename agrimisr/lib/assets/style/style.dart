0233a13ec13b3f2875b087ffb39e1fd7ed3cb212
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2022-07-17 12:07:12 +0200

    auth folder init

---
+++ agrimisr/lib/assets/style/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+//my colors class for the app
+class MyColors {
+  static const Color primary = Color.fromARGB(255, 93, 158, 55);
+  static const Color primaryDark = Color.fromARGB(255, 66, 87, 72);
+  static const Color secondary = Color.fromARGB(255, 182, 138, 34);
+}
