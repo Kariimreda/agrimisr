dae985dca87e50cd9bd09f9cd54aff6a8cb0e246
Kariimreda <redakariim3@gmail.com>
2022-07-26 16:51:38 +0200

    trying to handle internet error and product

--- agrimisr/lib/Layout/Ui/Layout.dart
+++ agrimisr/lib/Layout/Ui/Layout.dart
@@ -18,11 +18,16 @@ class _LayoutState extends State<Layout> {
   var darwertext = ButtonControllers().drawerTextButton;
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Obx(() => SafeArea(
           child: Scaffold(
             drawer: Drawer(
-              backgroundColor: MyColors.primaryDark,
+              backgroundColor: MyColors.background,
               child: ListView(
                 children: [
                   DrawerHeader(child: Image.asset('assets/images/logo.png')),
---
+++ agrimisr/lib/Modules/internetError.dart
@@ -0,0 +1,33 @@
+// import 'package:agrimisr/style/my_colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:overlay_support/overlay_support.dart';
+//
+// class InternetError extends StatefulWidget {
+//   const InternetError({Key? key}) : super(key: key);
+//   static const String routeName = '/InternetError';
+//
+//   @override
+//   State<InternetError> createState() => _InternetErrorState();
+// }
+//
+// class _InternetErrorState extends State<InternetError> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: MyColors.background,
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           TextButton(
+//               onPressed: () {
+//                 toast('No Internet Connection !');
+//                 // RestartWidget.restartApp(context);
+//                 // hasInternet ? Get.to(HomePage()) : Get.to(InternetError());
+//               },
+//               child: Center(child: Text('No Internet Connection !')))
+//         ],
+//       ),
+//     );
+//   }
+// }
